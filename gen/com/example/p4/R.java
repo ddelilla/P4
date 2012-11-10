1ecbd3a6b50d179a7eec7d19835b61a2a2b33c22
Jason Harris <jasonharris1991@gmail.com>
2012-11-10 17:32:20 -0600

    Changed Background

--- gen/com/example/p4/R.java
+++ gen/com/example/p4/R.java
@@ -11,9 +11,11 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int heat_seeking_missile=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int color2cropped=0x7f020000;
+        public static final int heat_seeking_missile=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int main_menu_background2=0x7f020004;
     }
     public static final class id {
         public static final int high_score_button=0x7f070004;
